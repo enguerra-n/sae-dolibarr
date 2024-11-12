f3859cd7719d5c2106893ebca6cc003a4a5aa598
enguerra-n <180629491+enguerra-n@users.noreply.github.com>
2024-11-12 11:22:00 +0100

    Add files via upload

---
+++ scripts/list_table.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Variables de configuration
+DB_USER="root"           # Nom d'utilisateur MariaDB
+DB_PASSWORD="root"      # Mot de passe MariaDB
+DB_NAME="dolibarr"      # Nom de la base de données (dolibarr dans ce cas)
+
+# Commande pour afficher les tables
+echo "Liste des tables dans la base de données $DB_NAME :"
+mariadb -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" -e "SHOW TABLES;" | tail -n +2
