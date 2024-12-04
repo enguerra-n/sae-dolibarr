37366a8e3f73497a88c9adef758d16beb7e19212
enguerra-n <larminateur@gmail.com>
2024-12-04 10:23:12 +0100

    ajout def

--- scripts/list_table.sh
+++ scripts/list_table.sh
@@ -8,3 +8,5 @@ DB_NAME="dolibarr"      # Nom de la base de données (dolibarr dans ce cas)
 # Commande pour afficher les tables
 echo "Liste des tables dans la base de données $DB_NAME :"
 mariadb -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" -e "SHOW TABLES;" | tail -n +2
+
+# le tail permet d'afficher à partir de la deuxième ligne
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,47 +1,63 @@
 #!/bin/bash
 
-# Variables de configuration
-DB_USER="root"           # Nom d'utilisateur MariaDB
-DB_PASSWORD="root"      # Mot de passe MariaDB
-DB_NAME="dolibarr"       # Nom de la base de données
+#######
+# Initialisation des variables
+#######
+DB_USER="root"           
+DB_PASSWORD="root"       
+DB_NAME="dolibarr"       
 
+#####
 # Fonction d'importation
+#####
 import_csv_to_mariadb() {
-    local table_name=$1
-    local csv_file=$2
+    local table_name=$1    
+    local csv_file=$2      
 
-    echo "Importation du fichier $csv_file vers la table $table_name ..."
+    echo "Importation du fichier $csv_file vers la table $table_name ..." 
+
+
     #mariadb --local-infile=1 -u root -p dolibarr --password=root
+    
     mariadb --local-infile=1 -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" -e "
-        LOAD DATA LOCAL INFILE '$csv_file'
+        LOAD DATA LOCAL INFILE '$csv_file' 
         INTO TABLE $table_name
-        FIELDS TERMINATED BY ',' 
-        ENCLOSED BY '\"' 
-        LINES TERMINATED BY '\n'
-        IGNORE 1 ROWS;
+        FIELDS TERMINATED BY ','  # Définit la virgule comme séparateur de champs dans le fichier CSV
+        ENCLOSED BY '\"'         # Définit les guillemets comme délimiteurs de champs dans le fichier CSV
+        LINES TERMINATED BY '\n' # Définit le saut de ligne comme délimiteur de lignes
+        IGNORE 1 ROWS;          # Ignore la première ligne (en-têtes de colonnes du fichier CSV)
     "
 
-    if [ $? -eq 0 ]; then
-        echo "Importation du fichier $csv_file vers $table_name réussie."
+    if [ $? -eq 0 ]; then   # Vérifie si la commande précédente (importation) a réussi (code de sortie égal à 0)
+        echo "Importation du fichier $csv_file vers $table_name réussie." 
+        # Si l'importation a réussi, affiche un message de succès.
     else
-        echo "Erreur lors de l'importation du fichier $csv_file vers $table_name."
+        echo "Erreur lors de l'importation du fichier $csv_file vers $table_name." 
+        # Si l'importation a échoué, affiche un message d'erreur.
     fi
 }
 
+#######
 # Vérification et extraction des arguments
+#######
 while getopts "t:c:" opt; do
   case $opt in
-    t) table_name="$OPTARG" ;;
-    c) csv_file="$OPTARG" ;;
-    *) echo "Usage: $0 -t table_name -c csv_file" >&2; exit 1 ;;
+    t) table_name="$OPTARG" ;;  # Si l'option -t est passée, affectation de la valeure à la variable
+    c) csv_file="$OPTARG" ;;    # Si l'option -c est passée, affectation de la valeure à la variable
+    *) echo "Usage: $0 -t table_name -c csv_file" >&2; exit 1 ;; 
+    # Si une option non valide est passée, affiche un message d'erreur et quitte le script
   esac
 done
 
-# Vérification que les arguments sont fournis
+
+
 if [ -z "$table_name" ] || [ -z "$csv_file" ]; then
     echo "Usage: $0 -t table_name -c csv_file" >&2
     exit 1
 fi
 
-# Appel de la fonction d'importation avec les arguments fournis
-import_csv_to_mariadb "$table_name" "$csv_file"
+
+####
+# programme principal
+####
+import_csv_to_mariadb "$table_name" "$csv_file" 
